869d9ffaae641020841eabfb4a2b0080dedc85b9
Duy Nguyen <duy210697@gmail.com>
2019-02-16 16:37:37 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.props.onClick(this.props.displayKey);
+  }
+
+  render() {
+    return (
+      <button style={this.props.style} className="w-100" onClick={this.onClick}>
+        {this.props.displayKey}
+      </button>
+    );
+  }
+}
+
+const Display = (props) => {
+  return (
+    <div id="calculator" className="d-flex flex-column justify-content-end text-right">
+      <div className="small-display">
+        {props.expression}
+      </div>
+      <div className="display">
+        {props.result}
+      </div>
+    </div>
+  );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expression: '0',
+      result: 0
+    };
+    this.onClickNumber = this.onClickNumber.bind(this);
+    this.onClickReset = this.onClickReset.bind(this);
+    this.onClickOperator = this.onClickOperator.bind(this);
+  }
+
+  onClickNumber(number) {
+    this.setState((prevState) => {
+      if (prevState.expression == '0') {
+        return {
+          expression: number,
+          result: number
+        };
+      }
+      return {
+        expression: prevState.expression + number,
+        result: prevState.expression + number
+      };
+    });
+  }
+
+  onClickOperator(op) {
+    this.setState((prevState) => {
+      return {
+        expression: prevState.expression + op,
+        result: op
+      };
+    });
+  }
+
+  onClickReset() {
+    this.setState({
+      expression: '0',
+      result: 0
+    });
+  }
+
+  render() {
+    return (
+      <div className="App-header d-flex justify-content-center align-items-center">
+        <table className="p-2">
+          <tbody>
+            <tr>
+              <td colSpan="4">
+                <Display expression={this.state.expression} result={this.state.result} />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <Button onClick={this.onClickReset} displayKey="AC" style={{ background: '#5b9eb3' }} />
+              </td>
+              <td>
+                <Button onClick={this.onClickOperator} displayKey="/" style={{ background: '#e2a025' }} />
+              </td>
+              <td>
+                <Button onClick={this.onClickOperator} displayKey="x" style={{ background: '#e2a025' }} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="7" />
+              </td>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="8" />
+              </td>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="9" />
+              </td>
+              <td>
+                <Button onClick={this.onClickOperator} displayKey="-" style={{ background: '#e2a025' }} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="4" />
+              </td>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="5" />
+              </td>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="6" />
+              </td>
+              <td>
+                <Button onClick={this.onClickOperator} displayKey="+" style={{ background: '#e2a025' }} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="1" />
+              </td>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="2" />
+              </td>
+              <td>
+                <Button onClick={this.onClickNumber} displayKey="3" />
+              </td>
+              <td rowSpan="2">
+                <Button displayKey="=" style={{ height: 132, background: '#458dcc' }} />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <Button onClick={this.onClickNumber} displayKey="0" />
+              </td>
+              <td>
+                <Button displayKey="." />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
